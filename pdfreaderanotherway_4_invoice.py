f2e687e9ef9a3fa50aa7e9aa429bf81546a4c7a9
Sugi <bcomsugi@yahoo.com>
2023-11-14 01:34:15 +0700

    float fix

--- pdfreaderanotherway_4_invoice.py
+++ pdfreaderanotherway_4_invoice.py
@@ -84,7 +84,6 @@ for page_layout in extract_pages(filename):
                             if bbox_y_price > textline.bbox[1]:
                                 bbox_y_price = textline.bbox[1]
                                 print( "price->", textline,  'price')
-                                
                                 prices.append(convertRPToFloat(textline.get_text().split('\n')[0].strip()))
                             else:
                                 pass
@@ -109,59 +108,10 @@ for page_layout in extract_pages(filename):
                         elif 425 < int(textline.bbox[0]) < 565 and  130 < int(textline.bbox[1]) < 145 :
                             print( "grandTotal ->", textline,  'grandTotal')
                             grandTotal = textline.get_text().split('\n')[0].split('Rp')[1].strip()
-                            print(convertRPToFloat(grandTotal),)
+                            grandTotal = convertRPToFloat(grandTotal)
+                            print(grandTotal)
                             
 
-                        # elif 363 < int(textline.bbox[0]) <368:
-                        #     if textline.get_text().split('\n')[0].split(' ')[1].strip().startswith("TCO-DN"):
-                        #         # RefNum.append(textline.get_text().split('\n')[0].split(' ')[1].strip())
-                        #         RefNum = textline.get_text().split('\n')[0].split(' ')[1].strip() # from TCO-DNPG-2305-00305
-                        #         RefNum = f'{RefNum.split("-")[-3][-1]}{RefNum.split("-")[-2]}{RefNum.split("-")[-1]}'   # into G230500305
-                        #     elif textline.get_text().split('\n')[0].split(' ')[1].strip().startswith("20"):
-                        #         TxnDate = textline.get_text().split('\n')[0].split(' ')[1].strip()
-
-                        # if 43<int(textline.bbox[0])<47:
-                        #     if bbox_y_item > textline.bbox[1]:
-                        #         bbox_y_item = textline.bbox[1]
-                        #         print( textline,  'ITEM')
-                        #         if not textline.get_text().startswith("Sub") and not textline.get_text().startswith("Jenis") and not textline.get_text().startswith("Item") and not textline.get_text().startswith("Page") and not textline.get_text().startswith("Printed"):
-                        #             # if textline.get_text().split('\n')[0].strip() == "TH-002AA":
-                        #             #     items.append('TH')
-                        #             if textline.get_text().split('\n')[0].strip() == "TPB-18.":
-                        #                 items.append('TPB-18')
-                        #             else:
-                        #                 items.append(textline.get_text().split('\n')[0].strip())
-                        #     else:
-                        #         pass
-                        #         print("Item Not in order")
-                        # elif 275 < int(textline.bbox[0]) < 290 and not textline.get_text().startswith("Quantity"):
-                        #     if bbox_y_qty > textline.bbox[1]:
-                        #         bbox_y_qty = textline.bbox[1]
-                        #         print( textline,  'QUANTITY')
-                        #         qtylist.append(float(textline.get_text().split('\n')[0].strip().split(' ')[0].strip()))
-                        #         uomlist.append(textline.get_text().split('\n')[0].strip().split(' ')[1].strip())
-                        #     else:
-                        #         pass
-                        #         print("QTY Not in order")
-                        # elif 375<int(textline.bbox[0])<379:
-                        #     if bbox_y_tco > textline.bbox[1]:
-                        #         bbox_y_tco = textline.bbox[1]
-                        #         if 472<int(textline.bbox[2])<475:
-                        #             print( textline,  'TCOSOL')
-                        #             tcolist.append(textline.get_text().split(' ')[0].strip())
-                        #         else:
-                        #             print( textline,  'PONUM')
-                        #             polist.append(textline.get_text().split('\n')[0].strip())
-                        #     else:
-                        #         pass
-                        #         print("TCO or PONo Not in order")
-                        # else:
-                        #     pass
-                        #     print( textline, textline.bbox)
-                        # # elif int(textline.bbox[0])==377 and int(textline.bbox[2])==:
-                        # #     print( textline, textline.bbox, 'TCOSOL')
-                        # # elif int(textline.bbox[0])==282:
-                        # #     print( textline, textline.bbox, 'QUANTITY')
     print(items, len(items))
     print(sjno, len(sjno))
     print(qtylist, len(qtylist))
@@ -173,28 +123,20 @@ print(f'items, {len(items)}')
 print(f'qtylist, {len(qtylist)}')
 print(f'tcolist, {len(tcolist)}')
 print(f'polist, {len(polist)}')
-# alllist=list(zip(items,items,qtylist, tcolist,tcolist , uomlist,polist))
-alllist=list(zip(items,items,qtylist, uomlist, prices, uomlist, prices))
+alllist=list(zip(items,items, sjno, qtylist, uomlist, prices))
 
-# print(alllist, len(alllist))
 for idx, x in enumerate(alllist):
     print(idx, x)
-# BillTo_1 = RefNum
 print(RefNum, BillTo_1,  BillTo_2, TxnDate)
 newdata = alllist
-DeliveryNotedict={'RefNum':RefNum, 'TxnDate':TxnDate, 'BillTo_1': BillTo_1, 'BillTo_2': BillTo_2}
+DeliveryNotedict={'RefNum':RefNum, 'TxnDate':TxnDate, 'BillTo_1': BillTo_1, 'BillTo_2': BillTo_2, 'GrandTotal': grandTotal}
 
-df=pd.DataFrame(newdata, columns=['Item No', 'Description', 'Quantity', 'No.SO/Ext.Doc.No.', 'LPN No.', 'UOM', 'Ext.Doc.No.'])#, columns=data[0]+"UOM")
+df=pd.DataFrame(newdata, columns=['Item No', 'Description', 'SJNo', 'Quantity', 'UOM', 'Price'])#, columns=data[0]+"UOM")
 # print(df)
-# df=df.groupby(['No.SO/Ext.Doc.No.','Item No', 'UOM'])['Quantity'].sum().reset_index().sort_values(by=['Item No', 'No.SO/Ext.Doc.No.'])#.sort_values(by=['No.SO/Ext.Doc.No.'])
-df=df.groupby(['Ext.Doc.No.', 'No.SO/Ext.Doc.No.','Item No', 'UOM'])['Quantity'].sum().reset_index().sort_values(by=['Ext.Doc.No.','No.SO/Ext.Doc.No.', 'Item No'])
 df['NameFromTaco']=df['Item No']
 print(df)
-# df['FullName'] = inteminvdf.loc[df['Item No'],'FullName']
 
 inteminvdf = pd.read_excel('ItemInventory140723.xlsx', usecols=['FullName', 'NameFromTaco'])
-# print(inteminvdf)
-# print (inteminvdf.index)
 
 df=df.merge(inteminvdf, how='left')
 print(df['FullName'].isnull().values.any())
@@ -207,8 +149,9 @@ if df['FullName'].isnull().sum() > 0:
     # return listitemNoFullName
 else:
     print(df)
-    df=df.groupby(['Ext.Doc.No.', 'No.SO/Ext.Doc.No.','Item No', 'UOM', 'FullName'])['Quantity'].sum().reset_index().sort_values(by=['Ext.Doc.No.','No.SO/Ext.Doc.No.', 'Item No'])
-    print(df)
+    df = df.astype({'Price': 'float'})
+    print(df.info())
+    # print(df)
     lst = df.to_dict('records')
     # print(lst)
     DeliveryNotedict['lines']=lst
