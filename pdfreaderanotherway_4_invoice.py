6467b5304aaf501b3783f6a72bb2f99192dbff32
Sugi <bcomsugi@yahoo.com>
2023-11-15 00:20:06 +0700

    update to multiplages

--- pdfreaderanotherway_4_invoice.py
+++ pdfreaderanotherway_4_invoice.py
@@ -8,19 +8,25 @@ filename = "example_001.pdf"
 filename = "TCO-DNPG-2303-0951.pdf"
 filename = "oriDNPG\TCO-DNPR-2305-00305.pdf"
 filename = "60974-03.pdf"
+filename = r"c:\Users\bcoms\Downloads\63178-27.pdf"
 items=[]
+descriptions=[]
 sjno=[]
 qtylist=[]
 uomlist=[]
 prices=[]
-
-tcolist=[]
-polist=[]
+itemLineNumber=[]
+sjnoLineNumber=[]
+qtyLineNumber=[]
+uomLineNumber=[]
+priceLineNumber=[]
 RefNum = None
 TxnDate = None
 BillTo_1 = None
 BillTo_2 = None
 grandTotal = None
+DIVIDER = 14.2
+
 
 def convertRPToFloat(data:str):
     koma = data.find(',')
@@ -32,32 +38,61 @@ def convertRPToFloat(data:str):
         data = data.replace(".", "")
         data = f'{data}.{decimal}'
         data = format(float(data), ".2f") #float(data)
-        print(data)
+        # print(data)
         return data
     else:
         print('salah')
     return None
-
+firstLineNumber = None
 for page_layout in extract_pages(filename):
     # print(type(page_layout), page_layout, page_layout.pageid)
     # if page_layout.pageid == 1:
+    
     if True:
         print(f'page:{page_layout.pageid}')
+        page = page_layout.pageid
         bbox_y_item = 10000
         bbox_y_uom = 10000
         bbox_y_price = 10000
         bbox_y_sjno = 10000
         bbox_y_qty = 10000
         bbox_y_tco = 10000
+        bbox_y_description = 10000
         for element in page_layout:
             if isinstance(element, LTTextContainer):
                 for textline in element:
+                    lineNumber = 0
                     if isinstance(textline, LTTextLineHorizontal):
                         if 10 < int(textline.bbox[0]) < 18 and 160 < int(textline.bbox[1]) < 560 :
                             if bbox_y_item > textline.bbox[1]:
                                 bbox_y_item = textline.bbox[1]
                                 print( "item->", textline,  'Item')
-                                items.append(textline.get_text().split('\n')[0].strip())
+                                # items.append(textline.get_text().split('\n')[0].strip())
+                                # lineNumber = 0
+                                if firstLineNumber:
+                                    lineNumber = firstLineNumber * page - int(textline.bbox[1]/14.2)
+                                    itemLineNumber.append(lineNumber)
+                                else:
+                                    firstLineNumber = int(textline.bbox[1]/14.2)
+                                    lineNumber = firstLineNumber-int(textline.bbox[1]/14.2)
+                                    itemLineNumber.append(lineNumber)
+                                items.append((lineNumber, textline.get_text().split('\n')[0].strip()))
+                            else:
+                                print("Item Not in order")
+
+                        if 125 < int(textline.bbox[0]) < 140 and 160 < int(textline.bbox[1]) < 560 :
+                            if bbox_y_description > textline.bbox[1]:
+                                bbox_y_description = textline.bbox[1]
+                                print( "desc->", textline,  'desc')
+                                # lineNumber = 0
+                                if firstLineNumber:
+                                    lineNumber = firstLineNumber * page -int(textline.bbox[1]/14.2)
+                                    # itemLineNumber.append(lineNumber)
+                                else:
+                                    firstLineNumber = int(textline.bbox[1]/14.2)
+                                    lineNumber = firstLineNumber-int(textline.bbox[1]/14.2)
+                                    # itemLineNumber.append(lineNumber)
+                                descriptions.append((lineNumber, textline.get_text().split('\n')[0].strip()))
                             else:
                                 print("Item Not in order")
 
@@ -65,7 +100,15 @@ for page_layout in extract_pages(filename):
                             if bbox_y_sjno > textline.bbox[1]:
                                 bbox_y_sjno = textline.bbox[1]
                                 print( "SJ No->", textline,  'SJ No')
-                                sjno.append(textline.get_text().split('\n')[0].strip())
+                                # lineNumber = 0
+                                if firstLineNumber:
+                                    lineNumber = firstLineNumber * page -int(textline.bbox[1]/14.2)
+                                    # itemLineNumber.append(lineNumber)
+                                else:
+                                    firstLineNumber = int(textline.bbox[1]/14.2)
+                                    lineNumber = firstLineNumber-int(textline.bbox[1]/14.2)
+                                    # itemLineNumber.append(lineNumber)
+                                sjno.append((lineNumber, textline.get_text().split('\n')[0].strip()))
                             else:
                                 print("SJ no Not in order")
 
@@ -73,10 +116,13 @@ for page_layout in extract_pages(filename):
                             if bbox_y_qty > textline.bbox[1]:
                                 bbox_y_qty = textline.bbox[1]
                                 print( "qty UOM->", textline,  'qty UOM')
-                                # qtylist.append(float(textline.get_text().split('\n')[0].strip().split(' ')[0].strip()))
-                                # uomlist.append(textline.get_text().split('\n')[0].strip().split(' ')[1].strip())
-                                qtylist.append(float(textline.get_text().split('\n')[0].split(' ')[0].strip()))
-                                uomlist.append(textline.get_text().split('\n')[0].split(' ')[1].strip())
+                                if firstLineNumber:
+                                    lineNumber = firstLineNumber * page -int(textline.bbox[1]/14.2)
+                                else:
+                                    firstLineNumber = int(textline.bbox[1]/14.2)
+                                    lineNumber = firstLineNumber-int(textline.bbox[1]/14.2)
+                                qtylist.append((lineNumber, float(textline.get_text().split('\n')[0].split(' ')[0].strip())))
+                                uomlist.append((lineNumber, textline.get_text().split('\n')[0].split(' ')[1].strip()))
                             else:
                                 print("qty UOM Not in order")
 
@@ -84,7 +130,12 @@ for page_layout in extract_pages(filename):
                             if bbox_y_price > textline.bbox[1]:
                                 bbox_y_price = textline.bbox[1]
                                 print( "price->", textline,  'price')
-                                prices.append(convertRPToFloat(textline.get_text().split('\n')[0].strip()))
+                                if firstLineNumber:
+                                    lineNumber = firstLineNumber * page -int(textline.bbox[1]/14.2)
+                                else:
+                                    firstLineNumber = int(textline.bbox[1]/14.2)
+                                    lineNumber = firstLineNumber-int(textline.bbox[1]/14.2)
+                                prices.append((lineNumber, convertRPToFloat(textline.get_text().split('\n')[0].strip())))
                             else:
                                 pass
                                 print("price Not in order")
@@ -110,29 +161,58 @@ for page_layout in extract_pages(filename):
                             grandTotal = textline.get_text().split('\n')[0].split('Rp')[1].strip()
                             grandTotal = convertRPToFloat(grandTotal)
                             print(grandTotal, type(grandTotal))
-                            
 
+                        print(textline)    
+    print(f'firstlinenumbber:{firstLineNumber * page}')
     print(items, len(items))
+    print(descriptions, len(descriptions))
     print(sjno, len(sjno))
     print(qtylist, len(qtylist))
     print(uomlist, len(uomlist))
     print(prices, len(prices))
-    print(tcolist, len(tcolist))
-    print(polist, len(polist))
+    print(itemLineNumber, len(itemLineNumber))
 print(f'items, {len(items)}')
 print(f'qtylist, {len(qtylist)}')
-print(f'tcolist, {len(tcolist)}')
-print(f'polist, {len(polist)}')
-alllist=list(zip(items,items, sjno, qtylist, uomlist, prices))
+# alllist=list(zip(items,descriptions, sjno, qtylist, uomlist, prices))
+templist = []
+def fillList(data, templist):
+    result = None
+    for _ in data:
+        if _[0] == idx:
+            templist.append(_[1])
+            result = True
+            break
+    else:
+        templist.append(None)
+    return templist, result
+alllist = []
+
+for idx in range(26*page):
+    result = [None, None, None, None, None, None]
+
+    templist=[idx]
+    templist, result[0] = fillList(items, templist)
+    # templist, result[1] = fillList(descriptions, templist)
+    templist, result[2] = fillList(sjno, templist)
+    templist, result[3] = fillList(qtylist, templist)
+    templist, result[4] = fillList(uomlist, templist)
+    templist, result[5] = fillList(prices, templist)
+    print(result)
+    if not all(v is None for v in result):
+        alllist.append(templist)
+
+
+print(alllist)
+
 
 for idx, x in enumerate(alllist):
     print(idx, x)
 print(RefNum, BillTo_1,  BillTo_2, TxnDate)
 newdata = alllist
 DeliveryNotedict={'RefNum':RefNum, 'TxnDate':TxnDate, 'BillTo_1': BillTo_1, 'BillTo_2': BillTo_2, 'GrandTotal': grandTotal}
-
-df=pd.DataFrame(newdata, columns=['Item No', 'Description', 'SJNo', 'Quantity', 'UOM', 'Price'])#, columns=data[0]+"UOM")
-# print(df)
+df = pd.DataFrame(newdata, columns=['lineno', 'Item No', 'SJNo', 'Quantity', 'UOM', 'Price'])
+# df=pd.DataFrame(newdata, columns=['Item No', 'Description', 'SJNo', 'Quantity', 'UOM', 'Price'])#, columns=data[0]+"UOM")
+print(df)
 df['NameFromTaco']=df['Item No']
 print(df)
 
@@ -157,3 +237,4 @@ else:
     DeliveryNotedict['lines']=lst
     print(DeliveryNotedict)
     # return deliveryNotedict
+print(df)
